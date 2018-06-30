516d8a43ee2a8b6cbb12c3517139e9cea04ff5e0
Eluehike Chiedu <e.chiedu@yahoo.com>
2018-06-30 12:39:18 +0200

    Update boto1.py

--- boto1.py
+++ boto1.py
@@ -1,23 +1,24 @@
 import boto3
+
 elb = boto3.client('elbv2')
 
-#Welcome print statement
-print (''' This program allows you to progrmatically view the health status of an instance in a target group, all you need are 3 inputs 
-           
+# Welcome print statement
+print(''' This program allows you to progrmatically view the health status of an instance in a target group, all you need are 3 inputs 
+
            TG-ARN: Target Group's ARN
-           
+
            Instance: Instance-id
-           
+
            Listener-Port: Listening-Port of Instance
-           
+
            ''')
 
-#variables input
+# variables input
 arn = input("What is the Target-groups' ARN: ")
 instance = input("What is the instance-id you would like to check for: ")
-listport = input ("What is the instance's listener port: ")
+listport = int(input("What is the instance's listener port: "))
 
-#boto3 elbv2 target health method
+# boto3 elbv2 target health method
 
 response = elb.describe_target_health(
     TargetGroupArn=arn,
@@ -29,8 +30,12 @@ response = elb.describe_target_health(
     ],
 )
 
-# boto3 elbv2 target health method produces a dictionary response, have no need of metadata key, so extracted only TargetHealthDescriptions
-print (response ['TargetHealthDescriptions'])
 
+# boto3 elbv2 target health method produces a dictionary response, have no need of metadata key, so extracted only TargetHealthDescriptions
+
+if ((((response ['TargetHealthDescriptions']) [0]) ['TargetHealth']) ['State']) == 'unhealthy':
+    print ( "Instance {} is currently {} because: {}".format (instance, ((((response ['TargetHealthDescriptions']) [0]) ['TargetHealth']) ['State']), ((((response ['TargetHealthDescriptions']) [0]) ['TargetHealth']) ['Reason'])))
+else:
+    print ( "Instance {} is currently {} ".format (instance, ((((response ['TargetHealthDescriptions']) [0]) ['TargetHealth']) ['State'])))
 
 
